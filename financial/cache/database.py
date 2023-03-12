6543d2b2f5b5b42c411f756e4904b93b7f992708
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-12 11:51:19 +0330

    delete OAUTH2_ACCESS_AUTH_TOKEN_EXPIRE_MINUTES env

--- financial/cache/database.py
+++ financial/cache/database.py
@@ -7,7 +7,6 @@ dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 CACHE_URL = os.getenv('CACHE_URL')
-AUTH_TOKEN_EXPIRE = int(os.getenv('OAUTH2_ACCESS_AUTH_TOKEN_EXPIRE_MINUTES')) * 60
 
 class RedisSingleton:
     _instance = None
