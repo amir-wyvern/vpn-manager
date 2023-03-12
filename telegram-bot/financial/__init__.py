11a7eff0202bbd59dbb98f99fd3996749e57adb7
amir-wyvern <amirhosein_wyvern@yahoo.com>
2023-03-12 11:53:33 +0330

    create new folder

---
+++ telegram-bot/financial/__init__.py
@@ -0,0 +1,7 @@
+import requests
+import os
+
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
+requests.post()
---
+++ telegram-bot/financial/wallet.py
@@ -0,0 +1,12 @@
+import requests
+
+class Wallet:
+
+    def __init__(self) -> None:
+        pass
+
+    def get_balance(self):
+        pass
+
+    def decrease_balance(self):
+        pass
